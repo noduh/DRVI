358b192644441917820df3ef3f15eb4c4e56d891
BuildTools <unconfigured@null.spigotmc.org>
2021-03-03 20:55:54 -0800

    Changes to the listener (can now constantly update without sending a code to stop the sound) Added a method with the purpose of changing the coordinates to match the bitmap coordinates (broken)

--- app/src/main/java/dev/noduh/drvi/MainActivity.java
+++ app/src/main/java/dev/noduh/drvi/MainActivity.java
@@ -6,9 +6,12 @@ import android.content.Context;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.util.Log;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "dev.noduh.drvi/MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +22,16 @@ public class MainActivity extends AppCompatActivity {
         int imageResource = getResources().getIdentifier("drvi_bwgradient", "drawable", MainActivity.this.getPackageName());
         map.setImageResource(imageResource);
 
+        // setting ScreenStuff variables and listeners
         ScreenStuff.bitmap = ((BitmapDrawable)map.getDrawable()).getBitmap(); // create bitmap and push it to ScreenStuff
-
-        SoundPlay.vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE); // setting the vibrator variable in SoundPlay to the right thing (must be done in MainActivity)
-
         ScreenStuff.setListeners(map);
+        ScreenStuff.leftPadding = map.getPaddingLeft();
+        ScreenStuff.topPadding = map.getPaddingTop();
+        Log.d(TAG, "width = " + map.getMaxWidth());
+        ScreenStuff.rightLimit = map.getMaxWidth() + ScreenStuff.leftPadding;
+        ScreenStuff.downLimit = map.getMaxHeight() + ScreenStuff.topPadding;
+
+        // setting SoundPlay variables
+        SoundPlay.vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE); // setting the vibrator variable in SoundPlay to the right thing (must be done in MainActivity)
     }
 }
--- app/src/main/java/dev/noduh/drvi/ScreenStuff.java
+++ app/src/main/java/dev/noduh/drvi/ScreenStuff.java
@@ -11,26 +11,58 @@ import android.widget.ImageView;
 public class ScreenStuff {
     private static final String TAG = "dev.noduh.drvi/ScreenStuff";
     public static Bitmap bitmap;
-
+    // leftPadding, topPadding, rightLimit, and downLimit will be used to convert to bitmap coordinates and help determine if it's in bounds
+    public static int leftPadding;
+    public static int topPadding;
+    public static int rightLimit;
+    public static int downLimit;
 
     @SuppressLint("ClickableViewAccessibility")
     public static void setListeners(View view) {
         view.setOnTouchListener((v, event) -> {
-            float x = event.getX();
-            float y = event.getY();
-            SoundPlay.transitionSound(getColor(x, y));
-            SoundPlay.transitionSound(-1); // stop the sound and no need to vibrate
-            return true;
+            switch (event.getAction()) { // test what action is happening
+                case MotionEvent.ACTION_DOWN: // if you touch the screen
+                case MotionEvent.ACTION_MOVE: // if you move your finger along it
+                    int x = (int) event.getX();
+                    int y = (int) event.getY();
+                    x = getBitmapCoords(x, "x");
+                    y = getBitmapCoords(y, "y");
+                    if (!(x == -1 && y == -1)) { // if it's in bounds
+                        SoundPlay.transitionSound(getColor(x, y));
+                    } else {
+                        SoundPlay.transitionSound(-1); // stop the sound and no need to vibrate
+                    }
+                    break; // and then break
+                case MotionEvent.ACTION_UP: // when you lift up your finger
+                    SoundPlay.transitionSound(-1); // stop the sound and no need to vibrate
+                    break;
+            }
+            return true; // done handling all the finger stuff
         });
     }
 
-    public static int getColor (float xFloat, float yFloat) {
-        int x = Math.round(xFloat); // set x an y values to integers
-        int y = Math.round(yFloat); // set x and y values to integers
-
+    public static int getColor (int x, int y) {
         int color = bitmap.getPixel(x, y); // returns the color without pre-multiplying
 
-        Log.d(TAG, "" + color); // logging color in Logcat (unimportant line)
+//        Log.d(TAG, "" + color); // logging color (unimportant line)
         return color;
+    }
+
+    public static int getBitmapCoords (int screenCoords, String xy) { // get the bitmap coordinates from the screen coordinates
+        int bitmapCoords = -1; // -1 is out of bounds
+        switch (xy) {
+            case "x":
+                if (leftPadding < screenCoords || screenCoords < rightLimit) { // if it's in bounds then continue
+                    bitmapCoords = screenCoords - leftPadding; // adjusting coordinates by taking out padding
+                }
+                break;
+            case "y":
+                if (topPadding < screenCoords || screenCoords < downLimit) { // if it's in bounds then continue
+                    bitmapCoords = screenCoords - topPadding; // adjusting coordinates by taking out padding
+                }
+                break;
+        }
+        Log.d(TAG, "\nscreenCoords " + xy + ": " + screenCoords + "\nbitmapCoords " + xy + ": " + bitmapCoords);
+        return bitmapCoords;
     }
 }
